49d775f7e23aacb2b5d64f7f929bc32ef7e56d59
Joel Joby <joeljobyp@gmail.com>
2024-09-01 01:11:45 +0530

    Update fix and Gemini integration

--- final_compiled_code.py
+++ final_compiled_code.py
@@ -59,7 +59,7 @@ def Initialize_Setup(email,driver,actions,password,subject,chapter_category,chap
     element = driver.find_element(By.XPATH, f"//*[contains(text(), '{test_name}')]")
     element.click()
 
-    print("Clicking the (View Results) Button ....")
+    print("Clicking the (View Results)(time-being) Start Test Button ....")
     time.sleep(2)
     element = driver.find_element(By.XPATH, "//*[contains(text(), 'View Results')]")
     element.click()
@@ -78,6 +78,7 @@ def fetch_question_text(driver):
     return eight.get_attribute("innerHTML")
 
 def fetch_options(driver):
+    all_options = []
     time.sleep(2)
     three = driver.find_element(By.ID, "xyz")
     four = three.find_element(By.TAG_NAME, "div")
@@ -92,142 +93,37 @@ def fetch_options(driver):
         div_2 = quill_view_html.find_element(By.TAG_NAME, "div")
         div_3 = div_2.find_element(By.TAG_NAME, "div")
         p = div_3.find_element(By.TAG_NAME, "p")
-        print("Options 1 : ",p.get_attribute("innerHTML"))
-    return options.get_attribute("innerHTML")
+        all_options.append(str(p.get_attribute("innerHTML")))
+        print("Option : ",p.get_attribute("innerHTML"))
+    return all_options
+
+def load_api_key():
+    with open('api_key.txt', 'r') as file:
+        api_key = file.read().strip()
+    return api_key
+
+def GenerateAnswer(question_text,options):
+    api_key = load_api_key()
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    prompt = f"{question_text} Below are the options : {options} give the option number which is correct (i.e 1 is correct, 2 is correct, 3 is correct, 4 is correct) but only give the number"
+    print("Prompt is : ",prompt)
+    response = model.generate_content(prompt,
+        generation_config=genai.types.GenerationConfig(
+        # Only one candidate for now.
+        candidate_count=1,
+        stop_sequences=["x"],
+        max_output_tokens=1,
+        temperature=1.0,
+    ),)
+    answer = response.text
+    print("the answer is options or : ",answer)
+    return answer
 
 
-# #this is to get the question div where the div is 
-# time.sleep(2)
-# one_div  = driver.find_element(By.CLASS_NAME, "ng-star-inserted")
-# two  = one_div.find_element(By.CLASS_NAME, "ng-star-inserted")
-# print(two.get_attribute("innerHTML"))
-
-# #performing recursive search to get the question text
-# three = driver.find_element(By.ID, "xyz")
-# four = three.find_element(By.TAG_NAME, "div")
-# four = four.find_element(By.TAG_NAME, "div")
-# five = four.find_element(By.TAG_NAME, "quill-view-html")
-# six = five.find_element(By.TAG_NAME, "div")
-# seven = six.find_element(By.TAG_NAME, "div")
-# eight = seven.find_element(By.TAG_NAME, "p")
-
-
-
-# #this is to get the question div where the div is 
-# time.sleep(2)
-# one_div  = driver.find_element(By.CLASS_NAME, "ng-star-inserted")
-# two  = one_div.find_element(By.CLASS_NAME, "ng-star-inserted")
-# print(two.get_attribute("innerHTML"))
-
-# #performing recursive search to get the question text
-# three = driver.find_element(By.ID, "xyz")
-# four = three.find_element(By.TAG_NAME, "div")
-# four = four.find_element(By.TAG_NAME, "div")
-# five = four.find_element(By.TAG_NAME, "quill-view-html")
-# six = five.find_element(By.TAG_NAME, "div")
-# seven = six.find_element(By.TAG_NAME, "div")
-# eight = seven.find_element(By.TAG_NAME, "p")
-
-    
-
-# cct_subject = "Chemistry"
-# email = "joelj26@deeple.in"
-# password = "joel225"
-# driver.get("https://learn.uat.deeple.in/")
-# time.sleep(5)
-# print("Currently waiting for the page to load")
-
-# #entering the email in the field 
-# input_field = driver.find_element(By.ID, 'ui-sign-in-email-input')
-# input_field.send_keys(email)
-# actions.send_keys(Keys.TAB).send_keys(Keys.ENTER).perform()
-# print("Entered email")
-
-# #clking the next button
-# time.sleep(5)
-# actions.send_keys(password).send_keys(Keys.TAB).send_keys(Keys.TAB).send_keys(Keys.ENTER).perform()
-
-# print("Waiting for site to load")
-# time.sleep(6)
-# courses_button = driver.find_element(By.CLASS_NAME, "mdc-tab__content")
-# courses_button.click()
-
-# print("clicking jee button")
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'JEE')]")
-# element.click()
-
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'Chemistry')]")
-# element.click()
-
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'Physical Chemistry (2)')]")
-# element.click()
-
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'Atomic Structure')]")
-# element.click()
-
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'CCT 04')]")
-# element.click()
-
-# time.sleep(2)
-# element = driver.find_element(By.XPATH, "//*[contains(text(), 'View Results')]")
-# element.click()
-
-# #this is to get the question div where the div is 
-# time.sleep(2)
-# one_div  = driver.find_element(By.CLASS_NAME, "ng-star-inserted")
-# two  = one_div.find_element(By.CLASS_NAME, "ng-star-inserted")
-# print(two.get_attribute("innerHTML"))
-
-# #performing recursive search to get the question text
-# three = driver.find_element(By.ID, "xyz")
-# four = three.find_element(By.TAG_NAME, "div")
-# four = four.find_element(By.TAG_NAME, "div")
-# five = four.find_element(By.TAG_NAME, "quill-view-html")
-# six = five.find_element(By.TAG_NAME, "div")
-# seven = six.find_element(By.TAG_NAME, "div")
-# eight = seven.find_element(By.TAG_NAME, "p")
-
-# final_question_text = eight.get_attribute("innerHTML")
-
-# print(eight.get_attribute("innerHTML"))
-
-
-# # third = three.find_element(By.TAG_NAME, "div")
-# #fourth = third.find_element(By.TAG_NAME, "div")
-
-
-
-
-# # Question_div = driver.find_element(By.CLASS_NAME, "ng-star-inserted")
-# # div_element = Question_div.find_element(By.TAG_NAME, "div")
-# # question_text = div_element.find_element(By.TAG_NAME, "div")
-# # try:
-# #     print("in try")
-# #     e = question_text.get_attribute("class")
-# #     print(e)
-# #     print("reached till here")
-# #     next = question_text.find_element(By.TAG_NAME, "div")
-# #     print("try ran")
-
-# # except:
-# #     print("in eceprt")
-# #     print(next)
-
-# # hello = question_text.find_element(By.TAG_NAME, "p")
-
-
-
-
-
-# #waiting for it to approve and make the password field visible
-
-# #Gemini API PART
-
+driver = webdriver.Chrome()
+actions = ActionChains(driver)
 Initialize_Setup(email,driver,actions,password,cct_subject,"Physical Chemistry (2)","Atomic Structure","CCT 04")
-fetch_question_text(driver)
-fetch_options(driver)
+question_text = fetch_question_text(driver)
+options = fetch_options(driver)
+GenerateAnswer(question_text,options)
